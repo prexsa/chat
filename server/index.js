bc1fdee7b7d0cf02dae269c4ac4ef3d5183616af
prexsamam <prexgit@gmail.com>
2022-10-04 23:43:09 -0700

    establish client/server socket connections

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
 const cors = require('cors');
+
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+  }
+});
 
 // middleware needs to be before routes
 // parse form data
@@ -12,4 +21,14 @@ app.use(cors());
 
 const PORT = process.env.PORT || 9000;
 
-app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
+io.on('connection', (socket) => {
+  socket.on('user', msg => {
+    console.log('msg: ', msg)
+    io.emit('user', msg)
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
+
+httpServer.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
+import { useState, useEffect, useRef } from 'react';
+import io  from "socket.io-client";
 import './App.css';
+// const socket = io(); // if FrontEnd on same domain as server
 
 function App() {
+  const [inputVal, setInputVal] = useState('');
+  // const [socket, setSocket] = useState(null);
+  // const [list, setList] = useState([]);
+  const socketRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socketRef.current.emit('user', inputVal)
+    setInputVal("");
+  }
+  const handleOnChange = (e) => {
+    setInputVal(e.target.value);
+  }
+
+  useEffect(() => {
+    socketRef.current = io("http://localhost:9000");
+
+    socketRef.current.on('user', function(msg) {
+      console.log('msg: ', msg)
+      // setList([...list, ...[msg]])
+    })
+    // close the connection
+    return () => socketRef.current.close();
+  }, []);
+
   return (
     <div className="app">
+      <div>
+        <form onSubmit={handleSubmit} >
+          <input type="text" className="input" placeholder="your username" value={inputVal} onChange={handleOnChange} />
+        </form>
+        <button>send</button>
+      </div>
+      {inputVal}
     </div>
   );
 }
