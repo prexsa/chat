f27c8a57d0e8ea42284c5db600e26eafe55c7a08
Prexsa <prexgit@gmail.com>
2024-02-05 12:09:47 -0800

    [ui] changed styling and logic for sidebar

--- src/components/chat/Sidebar.js
+++ src/components/chat/Sidebar.js
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable */
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import {
   Box,
@@ -13,6 +14,8 @@ import {
 import FolderIcon from '@mui/icons-material/Folder';
 import SearchIcon from '@mui/icons-material/Search';
 import AddIcon from '@mui/icons-material/Add';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 // import AddFriendRFH from './AddFriend.RFH';
 // import CreateGroup from './CreateGroup';
 // import Logout from '../Logout';
@@ -30,28 +33,51 @@ const movies = [
   { label: 'Pulp Fiction', year: 1994 },
 ];
 
-function generate(element) {
-  return [0, 1, 2].map((value) =>
-    React.cloneElement(element, {
-      key: value,
-    }),
-  );
-}
-
-// https://dribbble.com/shots/5478210-Desktop-Chat-Application
+// https://bashooka.com/inspiration/chat-ui-designs/
+// https://dribbble.com/shots/3407020-Messenger-Redesign/attachments/744131?mode=media
 // https://www.google.com/search?sca_esv=c8404836621fa050&rlz=1C1CHBF_enUS1056US1056&sxsrf=ACQVn08rRo3-Swk-6_liY9VZhYTFQ8YMmA:1706737299588&q=chat+desktop+designs+register+page&tbm=isch&source=lnms&sa=X&ved=2ahUKEwir4N3zy4iEAxUUMEQIHYAiBPcQ0pQJegQIDBAB&biw=1974&bih=1331&dpr=1#imgrc=rEGh0ln3B7hS3M&imgdii=9mUUjVIE-RBI_M
 
 const Sidebar = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleChannelSelect = (index) => {
+    console.log('handleChannelSelect, ', index);
+    setIsActive(index);
+  };
+
   return (
     <Box
       sx={{
-        padding: '10px',
+        // padding: '10px',
         borderRight: '1px solid #dedede',
         // width: 350,
         height: '100%',
+        backgroundColor: '#ffffff',
       }}
     >
-      <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          // justifyContent: 'space-between',
+          padding: '20px 15px',
+        }}
+      >
+        <AccountCircleIcon sx={{ fontSize: 30 }} />
+        <span>User</span>
+        <MoreVertIcon sx={{ marginLeft: 'auto' }} />
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'flex-end',
+          padding: '15px 10px',
+          // borderBottom: '1px solid',
+          // borderTop: '1px solid',
+          // backgroundColor: '#fbfcf8',
+          boxShadow: '1px 1px 4px 1px rgba(192,192,192,0.5)',
+        }}
+      >
         <SearchIcon
           sx={{
             color: 'action.active',
@@ -64,7 +90,11 @@ const Sidebar = () => {
           options={movies}
           sx={{ width: 250 }}
           renderInput={(params) => (
-            <TextField {...params} variant="standard" label="Search..." />
+            <TextField
+              {...params}
+              variant="standard"
+              label="Search contact..."
+            />
           )}
         />
         <AddIcon
@@ -74,27 +104,67 @@ const Sidebar = () => {
             border: '1px solid',
             borderRadius: '5px',
             fontSize: '30px',
+            boxShadow:
+              'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px',
             '&:hover': {
-              borderColor: 'primary.main',
-              color: 'primary.main',
+              // borderColor: 'primary.main',
+              // color: 'primary.main',
+              cursor: 'pointer',
+              boxShadow: '2px 2px 12px -2px rgba(0,0,0,0.75);',
             },
           }}
         />
       </Box>
       <List dense={false} sx={{ mt: 2 }}>
-        {generate(
-          <ListItem>
-            <ListItemAvatar>
-              <Avatar>
-                <FolderIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText
-              primary="Single-line item"
-              secondary={'Secondary text'}
-            />
-            <ListItemText primary="time" />
-          </ListItem>,
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17].map(
+          (value) => {
+            return (
+              <ListItem
+                key={value}
+                onClick={() => handleChannelSelect(value)}
+                sx={{
+                  alignItems: 'flex-start',
+                  color: '#2c333d',
+                  '&.Mui-selected': {
+                    backgroundColor: '#f8fbfc',
+                    boxShadow: '1px 1px 4px -2px rgba(0,0,0,0.75);',
+                    borderRight: '2px solid #2c84f7',
+                  },
+                  '&:hover': {
+                    cursor: 'pointer',
+                    backgroundColor: '#dcdcdc',
+                    boxShadow: '2px 6px 15px -2px rgba(0,0,0,0.75);',
+                  },
+                  '&:active': {
+                    backgroundColor: 'fbfbf9',
+                    boxShadow: '2px 6px 15px -2px rgba(0,0,0,0.75);',
+                  },
+                }}
+                selected={isActive === value}
+              >
+                <ListItemAvatar>
+                  <Avatar>
+                    <FolderIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary="Single-line item"
+                  secondary={'Secondary text'}
+                />
+                <ListItemText
+                  primary="time"
+                  sx={{
+                    marginLeft: 'auto',
+                    textAlign: 'right',
+                    justifyContent: 'top',
+                    color: '#2c333d',
+                    fontWeight: 500,
+                    fontSize: 16,
+                  }}
+                />
+              </ListItem>
+            );
+          },
         )}
       </List>
     </Box>
