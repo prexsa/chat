f10833b7142ea416719bc1863be22838391f6ceb
Prexsa <prexgit@gmail.com>
2024-02-14 15:16:00 -0800

    [ui] correctly display names to corresponding messages

--- src/components/chat/MessagePanel.js
+++ src/components/chat/MessagePanel.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { useContext, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { MessagesContext } from './Main';
@@ -5,20 +6,20 @@ import { Box, Typography, List, ListItem } from '@mui/material';
 // import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import ImageNotSupportedIcon from '@mui/icons-material/ImageNotSupported';
+// import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+// import ImageNotSupportedIcon from '@mui/icons-material/ImageNotSupported';
 
 const MessagePanel = ({
   user,
-  channel,
+  selectedRoom,
   // picture,
-  isGroup,
-  setShowModal,
-  extractAllImagesFromMessages,
+  // isGroup,
+  // setShowModal,
+  // extractAllImagesFromMessages,
 }) => {
   const bottomRef = useRef(null);
   const { messages, feedback } = useContext(MessagesContext);
-
+  console.log('user: ', selectedRoom);
   useEffect(() => {
     // console.log('picture: ', picture)
     // bottomRef.current?.scrollIntoView({block: "end", behavior: 'smooth'});
@@ -29,92 +30,126 @@ const MessagePanel = ({
     bottomRef.current?.scrollIntoView({ block: 'end', inline: 'nearest' });
   }, []);
 
-  const displayModal = (imgSrc) => {
+  /*const displayModal = (imgSrc) => {
     // console.log('displayModal; ')
     setShowModal(true);
     // setImages([imgSrc])
     extractAllImagesFromMessages(imgSrc, messages);
+  };*/
+
+  const renderUserMessage = (message) => {
+    return (
+      <ListItem
+        sx={{
+          display: 'flex',
+          alignItems: 'flex-start',
+          justifyContent: 'flex-end',
+          // columnGap: '5px',
+        }}
+      >
+        <Box>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'baseline',
+              justifyContent: 'flex-end',
+              columnGap: '5px',
+            }}
+          >
+            <Typography
+              variant="h6"
+              sx={{ color: '#2196f3', fontSize: '1rem' }}
+            >
+              You
+            </Typography>
+            <Typography variant="body1" sx={{ fontSize: 14, color: '#9e9e9e' }}>
+              at 10:30 AM
+            </Typography>
+          </Box>
+          <Typography variant="subtitles1" sx={{ color: '#616161' }}>
+            {message.message}
+          </Typography>
+        </Box>
+        <ListItemAvatar sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <Avatar alt="User" src="/static/images/avatar/2.jpg" />
+        </ListItemAvatar>
+      </ListItem>
+    );
+  };
+
+  const getUsername = (userId) => {
+    const name = selectedRoom.mates.filter((mate) => mate.userId !== userId);
+    return name[0].username;
+  };
+
+  const renderRoommatesMessage = (message) => {
+    const username = getUsername(message.userId);
+
+    return (
+      <ListItem sx={{ alignItems: 'flex-start' }}>
+        <ListItemAvatar>
+          <Avatar alt={username} src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <Box>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'baseline',
+              justifyContent: 'flex-start',
+              columnGap: '5px',
+            }}
+          >
+            <Typography
+              variant="h6"
+              sx={{ color: '#424242', fontSize: '1rem' }}
+            >
+              {username}
+            </Typography>
+            <Typography variant="body1" sx={{ fontSize: 14, color: '#9e9e9e' }}>
+              at 10:30 AM
+            </Typography>
+          </Box>
+          <Typography variant="subtitles1" sx={{ color: '#616161' }}>
+            {message.message}
+          </Typography>
+        </Box>
+      </ListItem>
+    );
   };
 
   return (
     <div className="message-box-container">
-      <ul className="chat">
-        <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
-          <ListItem sx={{ alignItems: 'flex-start' }}>
-            <ListItemAvatar>
-              <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-            </ListItemAvatar>
-            <Box>
-              <Box
-                sx={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  alignItems: 'baseline',
-                  justifyContent: 'flex-start',
-                  columnGap: '5px',
-                }}
-              >
-                <Typography
-                  variant="h6"
-                  sx={{ color: '#424242', fontSize: '1rem' }}
-                >
-                  Theresa Hudson
-                </Typography>
-                <Typography
-                  variant="body1"
-                  sx={{ fontSize: 14, color: '#9e9e9e' }}
-                >
-                  at 10:30 AM
-                </Typography>
-              </Box>
-              <Typography variant="subtitles1" sx={{ color: '#616161' }}>
-                Can&apos;t wait for dinner tonight, I&apos;m soo hungry!!!
-              </Typography>
-            </Box>
-          </ListItem>
+      <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+        {selectedRoom.messages.map((message, index) => {
+          console.log('messages: ', message);
+          return message.userId === user.userId
+            ? renderUserMessage(message)
+            : renderRoommatesMessage(message);
+        })}
 
-          <ListItem
-            sx={{
-              display: 'flex',
-              alignItems: 'flex-start',
-              justifyContent: 'flex-end',
-              // columnGap: '5px',
-            }}
-          >
-            <Box>
-              <Box
-                sx={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  alignItems: 'baseline',
-                  justifyContent: 'flex-end',
-                  columnGap: '5px',
-                }}
-              >
-                <Typography
-                  variant="h6"
-                  sx={{ color: '#2196f3', fontSize: '1rem' }}
-                >
-                  You
-                </Typography>
-                <Typography
-                  variant="body1"
-                  sx={{ fontSize: 14, color: '#9e9e9e' }}
-                >
-                  at 10:30 AM
-                </Typography>
-              </Box>
-              <Typography variant="subtitles1" sx={{ color: '#616161' }}>
-                Can&apos;t wait for dinner tonight, I&apos;m soo hungry!!!
-              </Typography>
-            </Box>
-            <ListItemAvatar
-              sx={{ display: 'flex', justifyContent: 'flex-end' }}
-            >
-              <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-            </ListItemAvatar>
-          </ListItem>
-        </List>
+        <ListItem ref={bottomRef} className="feedback-typing">
+          {feedback ? `typing...` : ''}
+        </ListItem>
+        <ListItem ref={bottomRef}></ListItem>
+      </List>
+    </div>
+  );
+};
+
+MessagePanel.propTypes = {
+  user: PropTypes.object,
+  selectedRoom: PropTypes.object,
+  isGroup: PropTypes.bool,
+  setShowModal: PropTypes.func,
+  extractAllImagesFromMessages: PropTypes.func,
+};
+
+export default MessagePanel;
+
+/*
+      <ul className="chat">
         {messages.map((message, idx) => {
           // console.log("message: ", message);
           // check message if isImage key exist
@@ -154,37 +189,16 @@ const MessagePanel = ({
               <div>
                 {isYou ? null : (
                   <div className="chat-username-txt">
-                    {isGroup ? message.username : channel.username}
+                    {isGroup ? message.username : selectedRoom.username}
                   </div>
                 )}
               </div>
             </li>
           );
         })}
-        {/*<li className="you">
-          <div className=" icon-message-container flex-direction-row">
-            <img
-              className="file-upload-image"
-              src={picture && picture}
-              alt=""
-            />
-          </div>
-        </li>*/}
         <li ref={bottomRef} className="feedback-typing">
           {feedback ? `typing...` : ''}
         </li>
         <li ref={bottomRef}></li>
       </ul>
-    </div>
-  );
-};
-
-MessagePanel.propTypes = {
-  user: PropTypes.object,
-  channel: PropTypes.object,
-  isGroup: PropTypes.bool,
-  setShowModal: PropTypes.func,
-  extractAllImagesFromMessages: PropTypes.func,
-};
-
-export default MessagePanel;
+      */
