13496132ec9566a2826a03ae27915f5eae69080a
prexsa <prexgit@gmail.com>
2025-11-10 14:58:18 -0800

    removed logout component from chat channellist sidebar

--- src/components/chat/Sidebar.js
+++ src/components/chat/Sidebar.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Box, Button, Typography } from '@mui/material';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { Box, Typography } from '@mui/material';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-// import CreateGroup from './CreateGroup';
 import ChannelList from './ChannelList';
-// import { useUserContext } from '../../context/userContext';
 
 const Sidebar = ({ user }) => {
-  // const { user } = useUserContext();
-
   return (
     <Box
       sx={{
-        // padding: '10px',
         borderRight: '1px solid #dedede',
-        // width: 350,
         height: '100%',
         backgroundColor: '#fdfdfe',
       }}
@@ -42,15 +35,6 @@ const Sidebar = ({ user }) => {
             {user.fname} {user.lname}
           </Typography>
         </Box>
-        <Button
-          id="basic-button"
-          aria-controls={open ? 'basic-menu' : undefined}
-          aria-haspopup="true"
-          aria-expanded={open ? 'true' : undefined}
-          // onClick={handleClick}
-        >
-          <MoreVertIcon sx={{ marginLeft: 'auto' }} />
-        </Button>
       </Box>
       <ChannelList user={user} />
     </Box>
